e8d4743a00c21f01ee4177dd04217bc1c53ce8b7
BugganeHuman <markizdelakarabas@gmail.com>
2025-10-12 20:21:42 +0500

    Update and rename index.html to Cashbox.java

---
+++ Cashbox.java
@@ -0,0 +1,9 @@
+package employee;
+
+public class Cashbox extends Product {
+   public static int money;
+
+   void showMoney() {
+       System.out.printf("Cashbox have %d $", money );
+   }
+    }
